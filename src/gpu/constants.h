b9f3e9c58cebdcf44d00472d7c43e0ece0eacfc3
Salvatore Lombardi <74710148+salbh@users.noreply.github.com>
2024-11-09 15:43:38 +0100

    Updated constants

--- src/gpu/constants.h
+++ src/gpu/constants.h
@@ -8,19 +8,20 @@ constexpr int ORDER_DESC = 0;  // Descending order
 
 // Bitonic Sort thread and element configuration
 // These constants define the number of threads and elements used for the Bitonic Sort algorithm
-constexpr int BITONIC_SORT_THREADS = 128;  // Number of threads to use in the Bitonic Sort kernel
+constexpr int BITONIC_SORT_THREADS = 384;  // Number of threads to use in the Bitonic Sort kernel
 // constexpr int ELEMENTS_BITONIC_SORT = 4;    // Number of elements processed per thread in Bitonic Sort
 constexpr int BITONIC_SORT_BLOCKS = 8192;
 
 // Global Merge thread and element configuration
 // These constants define the number of threads and elements for global merging during sorting
-constexpr int MERGE_GLOBAL_THREADS = 256;   // Number of threads to use in the global merge kernel
+constexpr int MERGE_GLOBAL_THREADS = 768;   // Number of threads to use in the global merge kernel
 // constexpr int ELEMENTS_GLOBAL_MERGE = 4;     // Number of elements processed per thread in global merge
 constexpr int MERGE_GLOBAL_BLOCKS = 4096;
 
 // Local Merge thread and element configuration
 // These constants define the number of threads and elements for local merging during sorting
-constexpr int MERGE_LOCAL_THREADS = 256;     // Number of threads to use in the local merge kernel
-constexpr int MERGE_LOCAL_ELEMENTS = 8;       // Number of elements processed per thread in local merge
+constexpr int MERGE_LOCAL_THREADS = 768;     // Number of threads to use in the local merge kernel
+// constexpr int ELEMENTS_LOCAL_MERGE = 8;       // Number of elements processed per thread in local merge
+constexpr int MERGE_LOCAL_BLOCKS = 4096;
 
 #endif // CONSTANTS_H
